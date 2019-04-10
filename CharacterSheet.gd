c63e438e6858532a1daefac9a25f27ebd4789dfa
FITZGERALD CHRISTIAN <150049668@cart.ads>
2019-04-10 12:53:56 -0700

    Added dynamic loading of characters

--- CharacterSheet.gd
+++ CharacterSheet.gd
@@ -4,3 +4,4 @@ class_name Character
 export var name: String = "Character Template"; 
 export var blurb: String = "Character Blurb";
 export var hand: PoolIntArray = PoolIntArray();
+export var id: int = 0;
--- CharacterSystem.gd
+++ CharacterSystem.gd
@@ -1,21 +1,32 @@
 extends Node
-var Barbara: Character = load("res://Characters/BarbaraB.tres");
-var Benjamin: Character = load("res://Characters/BenD.tres");
-var Natasha: Character = load("res://Characters/NatashaS.tres");
-var William: Character = load("res://Characters/WillY.tres");
-var Clyde: Character = load("res://Characters/ClydeS.tres");
-var list = [Barbara, Benjamin, Natasha, William, Clyde];
+
 var characters: Array = Array();
 
 var guilty_character: int = 0;
 
 const CHARDIR: String = "res:///Characters/";
 
-	
+func load_characters() -> void:
+	# Load all the characters
+	var dir: Directory = Directory.new();
+	if dir.open(CHARDIR) == OK:
+		# Skip . and .. and all hidden files
+		dir.list_dir_begin(true, true);
+		var file_name: String = dir.get_next();
+		while file_name != "":
+			var character: Character = load(CHARDIR + file_name);
+			if character.id >= characters.size():
+				characters.resize(character.id + 1);
+			characters[character.id] = character;
+			file_name = dir.get_next()
+	else:
+		printerr("Could not open the cards directory!");
+		get_tree().quit();
 
 func _ready():
 #using this to decide who's guilty to start with
 	randomize();
+	load_characters();
 	numb();
 	pass
 func numb():
